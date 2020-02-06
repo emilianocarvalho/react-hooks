1bc1820713b0e9ffc9c6eeba7760700fa43daf63
Emiliano Fernandes <emilianocarvalho.dev@gmail.com>
2020-02-05 22:52:48 -0300

    Refactoring prevent race condictions

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React, { useState, useEffect } from 'react';
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
-  useEffect(async () => {
-    const response = await fetch(
-      'https://api.github.com/users/emilianocarvalho/repos'
-    );
-    const data = await response.json();
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        'https://api.github.com/users/emilianocarvalho/repos'
+      );
+      const data = await response.json();
+      setRepositories(data);
+    }
 
-    setRepositories(data);
-  }, []); // array vazio vai indicar que ele execute apenas uma única vez
+    fetchData();
+  }, []); // Component Did Mount, vazio executa apenas uma vez
 
   function handleFavorite(id) {
     console.log(id);
